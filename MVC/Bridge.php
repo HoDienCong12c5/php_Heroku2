f12c764f2eba0a8a414c97e7667fd53c7c8c8f62
HoDienCong12c5 <hodiecong12c5@gmail.com>
2021-07-22 23:13:14 +0700

    coment Bridge.php

--- MVC/Bridge.php
+++ MVC/Bridge.php
@@ -1,7 +1,7 @@
 <?php 
-    require_once "MVC/core/App.php";
-    require_once "MVC/core/Controller.php"; 
-    require_once "MVC/core/DB.php"; 
-    require_once "MVC/controllers/Admin/UpDownPoint_Sale.php"; 
+    // require_once "MVC/core/App.php";
+    // require_once "MVC/core/Controller.php"; 
+    // require_once "MVC/core/DB.php"; 
+    // require_once "MVC/controllers/Admin/UpDownPoint_Sale.php"; 
     echo "Bridge.php";
  ?>
