dfe39704f7bf29787a8e43854410afe67e725896
HoDienCong12c5 <hodiecong12c5@gmail.com>
2021-07-22 23:02:23 +0700

    dsfds

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
     session_start(); 
-    require_once "MVC/Bridge.php";
-    $myApp = new App();
-    
+    // require_once "MVC/Bridge.php";
+    // $myApp = new App();
+    echo "Test";
 ?>
